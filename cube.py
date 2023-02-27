2cbcb32549159375862d36f90a1c38603eeaa211
Brian K. Chirchir <BChirchir@Cma.local>
2023-02-27 09:45:37 +0300

    Using function to confirm whether a year is leap or not

---
+++ cube.py
@@ -0,0 +1,7 @@
+def introduction(first_name, last_name):
+    print("Hello, my name is", first_name, last_name)
+ 
+introduction("Skywalker", "Luke")
+introduction("Quick", "Jesse")
+introduction("Kent", "Clark")
+ 
